4ff5e0c9dfdfef9f6c11789078f3c74694243b2b
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2023-02-17 00:28:53 -0800

    Changed name of Feedback and FeedbackReply views, added PUT and DELETE requests to Feedback and FeedbackReply views, changed handleLikesDislikes view to use L for likes and D for dislikes instead of integers

--- backend/feedback/urls.py
+++ backend/feedback/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.viewFeedback),
-    path('<int:feedback_id>', views.viewFeedbackReply), # path to GET children of feedback posts as feedback replies
-    path('<int:feedback_id>/handlereactions/<int:int>', views.handleLikesDislikes), # path to like or dislike a parent feedback post | int = 1 for likes and 2 for dislikes
-    path('<int:feedback_id>/<int:reply_to_feedback_id>/handlereactions/<int:int>', views.handleLikesDislikes), # path to like or dislike a feedback reply (child) post | int = 1 for likes and 2 for dislikes
+    path('', views.allFeedback),
+    path('<int:pk>', views.allFeedback), # path for DELETE
+    path('<int:feedback_id>/reply', views.allFeedbackReply), # path to GET children of feedback posts as feedback replies
+    path('<int:feedback_id>/reply/<int:pk>', views.allFeedbackReply), # path for DELETE
+    path('<int:feedback_id>/handlereactions/<str:str>', views.handleLikesDislikes), # path to like or dislike a parent feedback post | str = L for likes and D for dislikes
+    path('<int:feedback_id>/reply/<int:reply_to_feedback_id>/handlereactions/<str:str>', views.handleLikesDislikes), # path to like or dislike a feedback reply (child) post | str = L for likes and D for dislikes
 ]
--- backend/feedback/views.py
+++ backend/feedback/views.py
@@ -6,9 +6,9 @@ from .models import *
 from .serializers import *
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 @permission_classes([IsAuthenticatedOrReadOnly])
-def viewFeedback(request):
+def allFeedback(request, pk=0):
     if request.method == 'GET':
         feedback = Feedback.objects.all()
         serializer = FeedbackSerializer(feedback, many = True)
@@ -19,14 +19,33 @@ def viewFeedback(request):
             serializer.save(user = request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'PUT':
+        get_user_posts = Feedback.objects.filter(user__username = request.user.username)
+        feedback = Feedback.objects.filter(pk=pk)[0]
+        if (feedback in get_user_posts) or request.user.is_superuser:
+            serializer = FeedbackSerializer(feedback, data=request.data)
+            if serializer.is_valid():
+                serializer.save(user=feedback.user)
+                return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+    if request.method == 'DELETE':
+        delete = Feedback.objects.filter(pk=pk)[0]
+        get_user_posts = Feedback.objects.filter(user__username = request.user)
+        if (delete in get_user_posts) or request.user.is_staff:
+            delete.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
     
     
-@api_view(['GET', 'POST'])
+    
+@api_view(['GET', 'POST', 'DELETE', 'PUT'])
 @permission_classes([IsAuthenticatedOrReadOnly])
-def viewFeedbackReply(request, feedback_id):
+def allFeedbackReply(request, feedback_id, pk=0):
     if request.method == 'GET':
-        feedbackreply = FeedbackReply.objects.filter(reply_to_feedback_id = feedback_id)
-        serializer = FeedbackReplySerializer(feedbackreply, many = True)
+        feedbackReply = FeedbackReply.objects.filter(reply_to_feedback_id = feedback_id)
+        serializer = FeedbackReplySerializer(feedbackReply, many = True)
         return Response(serializer.data)
     if request.method == 'POST':
         serializer = FeedbackReplySerializer(data=request.data)
@@ -34,15 +53,33 @@ def viewFeedbackReply(request, feedback_id):
             serializer.save(user = request.user, reply_to_feedback_id = feedback_id)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'PUT':
+        get_user_posts = FeedbackReply.objects.filter(user__username = request.user.username)
+        feedbackReply = FeedbackReply.objects.filter(pk=pk)[0]
+        if (feedbackReply in get_user_posts) or request.user.is_superuser:
+            serializer = FeedbackReplySerializer(feedbackReply, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(user = feedbackReply.user, reply_to_feedback_id = feedback_id)
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+    if request.method == 'DELETE':
+        get_user_posts = FeedbackReply.objects.filter(user__username = request.user.username)
+        delete = FeedbackReply.objects.filter(pk=pk)[0]
+        if (delete in get_user_posts) or request.user.is_staff:
+            delete.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
     
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
-def handleLikesDislikes(request, feedback_id = 0, reply_to_feedback_id=0, int=0):       # Where int = 1 for likes and 2 for dislikes
+def handleLikesDislikes(request, feedback_id = 0, reply_to_feedback_id=0, str=''):       # Where int = 1 for likes and 2 for dislikes
     if request.method == 'PUT':
         user = User.objects.get(username=request.user.username)
         if reply_to_feedback_id == 0:   # if POST request is on a parent feedback object and NOT on a feedback reply object
             feedback = Feedback.objects.get(id=feedback_id)
-            if int == 1:    # if Like
+            if str == 'L':    # if Like
                 if feedback.disliked_users.contains(user):     # Check and remove if user already disliked post
                     feedback.disliked_users.remove(user)
                     feedback.dislikes -= 1
@@ -55,7 +92,7 @@ def handleLikesDislikes(request, feedback_id = 0, reply_to_feedback_id=0, int=0)
                 feedback.liked_users.add(user)
                 feedback.save()     
                 return Response(status=status.HTTP_204_NO_CONTENT)
-            elif int == 2:    # if Dislike
+            elif str == 'D':    # if Dislike
                 if feedback.liked_users.contains(user):     # Check and remove if user already liked post
                     feedback.liked_users.remove(user)
                     feedback.likes -= 1
@@ -70,7 +107,7 @@ def handleLikesDislikes(request, feedback_id = 0, reply_to_feedback_id=0, int=0)
                 return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             feedback_reply = FeedbackReply.objects.get(id=reply_to_feedback_id)
-            if int == 1:    # if like
+            if str == 'L':    # if like
                 if feedback_reply.disliked_users.contains(user):    # Check and remove if user already disliked post
                     feedback_reply.disliked_users.remove(user)
                     feedback_reply.dislikes -= 1
@@ -83,7 +120,7 @@ def handleLikesDislikes(request, feedback_id = 0, reply_to_feedback_id=0, int=0)
                 feedback_reply.liked_users.add(user.id)
                 feedback_reply.save()
                 return Response(status=status.HTTP_204_NO_CONTENT)
-            elif int == 2:      # if dislike
+            elif str == 'D':      # if dislike
                 if feedback_reply.liked_users.contains(user):   # Check and remove if user already liked post
                     feedback_reply.liked_users.remove(user)
                     feedback_reply.likes -= 1
